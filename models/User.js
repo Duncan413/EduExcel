d1f03d430bcaf9d34da9207dd6fe596bfed412f6
Onkabetse Monamodi <168908077+Duncan413@users.noreply.github.com>
2024-10-13 16:17:05 +0200

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,9 +1,15 @@
+// models/User.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { 
+        type: String, 
+        enum: ['admin', 'teacher', 'student'], // Defining allowed roles
+        default: 'student' // Default role is student
+    }
 });
 
 module.exports = mongoose.model('User', UserSchema);
