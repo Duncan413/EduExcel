3ba74ef98883cc2fd39c19b18f1748a0881ee9ba
Onkabetse Monamodi <168908077+Duncan413@users.noreply.github.com>
2024-10-13 16:20:20 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 // Load config
 dotenv.config();
@@ -13,8 +14,17 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// Enable CORS for all routes
+app.use(cors());
+
 // Routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/quiz', require('./routes/quiz')); // This handles quiz-related routes
+const classRoutes = require('./routes/class');
+app.use('/api/class', classRoutes);
+const adminRoutes = require('./routes/admin');
+app.use('/api/admin', adminRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
